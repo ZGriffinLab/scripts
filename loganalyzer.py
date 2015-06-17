ae559333af47642e3d0693dfbb2a84acf221fc00
Ariel Sibille <arielsibille@gmail.com>
2015-06-17 05:43:47 -0500

    Attempt to fix logcombiner
    Realized logcombiner was broken but have not fixed it. Loganalyzer also
    not working.

--- loganalyzer.py
+++ loganalyzer.py
@@ -10,23 +10,41 @@ import re
 
 def loganalyzer(filename, outputname) :
 	#create a csv writer that will write to a file with our output name
-	w = csv.writer(open(outputname,'wb'), delimiter=',')
+	w = csv.writer(open(outputname,'wt'), delimiter=',')
 	#create a reader to read from our csv file
 	reader = csv.reader(open(filename, 'rU'), delimiter = ',')
 
 	#grab first line and all of our indeces
-	firstline_index= reader.next()
+	firstline = next(reader)
+	trial_index_index = firstline.index('trial')
 	transition_index = firstline.index('forward order')
 	speech_onset_index = firstline.index('speech onset')
 	participant_index = firstline.index('participant')
+	rt_index = firstline.index('response time')
 
-	#initialize our last line as the first line
-	lastline = firstline
 	#write out the column headings
-	w.writerow(firstline + ['transitions before onset', 'average biased', 'average unbiased'])
+	w.writerow(firstline + ['transitions before onset', 'transitions after onset', 'average biased', 'average unbiased'])
+
+	lastline = firstline
+	trans_before = 0
+	trans_after = 0
+	lines = 0
+	avg_biased = 0
+	avg_unbiased = 0
+
 	#go through the csv file
 	for line in reader :
-
+		if lastline != firstline :
+			if line[rt_index] < line[speech_onset_index] :
+				trans_before = trans_before + int(line[transition_index])
+			else :
+				trans_after = trans_after + int(line[transition_index])
+			#if there's a new parcticipant
+			if line[participant_index] != lastline[participant_index] :
+				newline = [lastline + [trans_before, trans_after, avg_biased, avg_unbiased]]
+				w.writerow(newline)
+			else:
+				w.writerow(lastline)
 		lastline = line
 
 #here's the actual method call
--- logcombiner.py
+++ logcombiner.py
@@ -11,65 +11,72 @@ import os
 
 def logcombiner(filename, outputname) :
 
-	#create a csv writer that will write to a file with our output name
-	w = csv.writer(open(outputname,'w', newline=''), delimiter=',')
+    #create a csv writer that will write to a file with our output name
+    w = csv.writer(open(outputname,'w', newline=''), delimiter=',')
 
-	#create a reader to read from our csv file
-	reader = csv.reader(open(filename, 'rU'), delimiter = ',')
+    #create a reader to read from our csv file
+    reader = csv.reader(open(filename, 'rU'), delimiter = ',')
 
-	#we'll be looking for exp data files so we compile this
-	p = re.compile('exp_data')
+    #we'll be looking for exp data files so we compile this
+    p = re.compile('exp_data')
 
-	#get the first line in the csv file
-	currline = reader.__next__()
-	#get the index of the image file field in the csv file
-	imgindex = currline.index('imgfile')
-	labelindex = currline.index('CURRENT_FIX_INTEREST_AREA_LABEL')
-	#get the first line of actual data from the csv file
-	currline = reader.__next__()
+    #get the first line in the csv file
+    currline = reader.__next__()
+    #get the index of the image file field in the csv file
+    imgindex = currline.index('imgfile')
+    labelindex = currline.index('CURRENT_FIX_INTEREST_AREA_LABEL')
+    #get the first line of actual data from the csv file
+    currline = reader.__next__()
 
-	#go through the participants folder
-	for dirname in os.listdir("../experiment files/participants") :
-		#join the directory name to the rest of the path
-		directory = os.path.join('../experiment files/participants', dirname)
-		#we don't want to try to navigate through something in the participants folder that isn't a dir
-		if os.path.isdir(directory) :
-			#for every participants folder, we look for the exp data file
-			for file in os.listdir(directory) :
-				#if the file is the exp data file
-				if p.search(file) :
-					#create a reader to read from our exp data file
-					logreader = open(os.path.join(directory, file), 'rU')
-					#splitting the current line by tabs
-					currlogline = logreader.readline().split('\t')
+    first = 1
 
-					#finding indeces of all the good stuff that we want
-					logimgindex = currlogline.index('stim_file_name')
-					biasedindex = currlogline.index('biased')
-					onsetindex = currlogline.index('response time')
+    #go through the participants folder
+    for dirname in os.listdir("../experiment files/participants") :
+        #join the directory name to the rest of the path
+        directory = os.path.join('../experiment files/participants', dirname)
+        #we don't want to try to navigate through something in the participants folder that isn't a dir
+        if os.path.isdir(directory) :
+            #for every participants folder, we look for the exp data file
+            for file in os.listdir(directory) :
+                #if the file is the exp data file
+                if p.search(file) :
+                    #create a reader to read from our exp data file
+                    logreader = open(os.path.join(directory, file), 'rU')
+                    #splitting the current line by tabs
+                    currlogline = logreader.readline().split('\t')
 
-					w.writerow(currlogline[:-2] + ['biased','speech onset', 'forward order'])
+                    #finding indeces of all the good stuff that we want
+                    logimgindex = currlogline.index('stim_file_name')
+                    biasedindex = currlogline.index('biased')
+                    onsetindex = currlogline.index('response time')
 
-					currlogline = logreader.readline().split('\t')
+                    if first :
+                        w.writerow(currlogline[:-2] + ['biased','speech onset', 'forward order'])
+                        first = 0
+                    else :
+                        currline = reader.__next__()
 
-					#go through the exp data file
-					for line in logreader :
-						lastlogline = currlogline
-						currlogline = line.split('\t')
-						#if the image files match, minus the extensions
-						try:
-							while currline[imgindex][:-3] == lastlogline[logimgindex][:-3] :
-								lastline = currline
-								currline = reader.__next__()
-								forwardorder = 0
-								if (lastline[labelindex] == 'patient' and currline[labelindex] == 'recipient') or (lastline[labelindex] == 'recipient' and currline[labelindex] == 'patient') :
-									forwardorder = 1
-								newline = lastline[:-1] + [lastlogline[biasedindex], lastlogline[onsetindex], forwardorder]
-								w.writerow(newline)
-						#have to catch reader's stopiteration exception
-						except Exception as e:
-							pass
+                    currlogline = logreader.readline().split('\t')
+
+                    #go through the exp data file
+                    for line in logreader :
+                        lastlogline = currlogline
+                        currlogline = line.split('\t')
+                        print(currline)
+                        #if the image files match, minus the extensions
+                        try:
+                            while currline[imgindex][:-3] == lastlogline[logimgindex][:-3] :
+                                lastline = currline
+                                currline = reader.__next__()
+                                forwardorder = 0
+                                if (lastline[labelindex] == 'patient' and currline[labelindex] == 'recipient') or (lastline[labelindex] == 'recipient' and currline[labelindex] == 'patient') :
+                                    forwardorder = 1
+                                    newline = lastline[:-1] + [lastlogline[biasedindex], lastlogline[onsetindex], forwardorder]
+                                    w.writerow(newline)
+                                    #have to catch reader's stopiteration exception
+                        except Exception as e:
+                            pass
 
 #here's the actual method call
 if __name__ == '__main__':
-	logcombiner("results.csv", "combinedlog.csv")
+    logcombiner("results.csv", "combinedlog.csv")
