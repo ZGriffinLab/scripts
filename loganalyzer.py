b9d4f5fcfc03dd048b28f75367127d17968010f2
Ariel Sibille <arielsibille@gmail.com>
2015-07-11 16:31:42 -0500

    Loganalyzer transitions done
    Loganalyzer.py working for transitions before and after speech onset,
    calculated using gaze end

--- loganalyzer.py
+++ loganalyzer.py
@@ -10,17 +10,19 @@ import re
 
 def loganalyzer(filename, outputname) :
 	#create a csv writer that will write to a file with our output name
-	w = csv.writer(open(outputname,'wt'), delimiter=',')
+	newfile = open(outputname,'wt')
+	w = csv.writer(newfile, delimiter=',')
 	#create a reader to read from our csv file
-	reader = csv.reader(open(filename, 'rU'), delimiter = ',')
+	oldfile = open(filename, 'rU')
+	reader = csv.reader(oldfile, delimiter = ',')
 
 	#grab first line and all of our indeces
 	firstline = next(reader)
-	trial_index_index = firstline.index('trial')
+	trial_index_index = firstline.index('TRIAL_INDEX')
 	transition_index = firstline.index('forward order')
 	speech_onset_index = firstline.index('speech onset')
-	participant_index = firstline.index('participant')
-	rt_index = firstline.index('response time')
+	participant_index = firstline.index('RECORDING_SESSION_LABEL')
+	gaze_end_index = firstline.index('CURRENT_GAZE_END')
 
 	#write out the column headings
 	w.writerow(firstline + ['transitions before onset', 'transitions after onset', 'average biased', 'average unbiased'])
@@ -31,22 +33,29 @@ def loganalyzer(filename, outputname) :
 	lines = 0
 	avg_biased = 0
 	avg_unbiased = 0
+	count = 0
 
 	#go through the csv file
 	for line in reader :
 		if lastline != firstline :
-			if line[rt_index] < line[speech_onset_index] :
-				trans_before = trans_before + int(line[transition_index])
+			count += 1
+			if line[gaze_end_index] < line[speech_onset_index] :
+				trans_before += int(line[transition_index])
 			else :
-				trans_after = trans_after + int(line[transition_index])
+				trans_after += int(line[transition_index])
 			#if there's a new parcticipant
 			if line[participant_index] != lastline[participant_index] :
-				newline = [lastline + [trans_before, trans_after, avg_biased, avg_unbiased]]
+				newline = lastline + [trans_before, trans_after, avg_biased, avg_unbiased]
 				w.writerow(newline)
+				trans_before = 0
+				trans_after = 0
 			else:
 				w.writerow(lastline)
 		lastline = line
 
+	newfile.close()
+	oldfile.close()
+
 #here's the actual method call
 if __name__ == '__main__':
-	loganalyzer("combinedlog.csv", "analyzedlog.csv")
+	loganalyzer("forwardorder.csv", "analyzedlog.csv")
