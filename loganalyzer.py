04afe924e5513bbc74ef764e9115c3d29501992a
Ariel Sibille <arielsibille@gmail.com>
2015-07-11 16:42:07 -0500

    Add biased and unbiased for loganalyzer
    Avg number of transitions for biased and unbiased trials added,
    computed by getting number of transitions for each type of trial and
    dividing them by number each type of trial

--- loganalyzer.py
+++ loganalyzer.py
@@ -23,32 +23,44 @@ def loganalyzer(filename, outputname) :
 	speech_onset_index = firstline.index('speech onset')
 	participant_index = firstline.index('RECORDING_SESSION_LABEL')
 	gaze_end_index = firstline.index('CURRENT_GAZE_END')
+	biased_index = firstline.index('biased')
 
 	#write out the column headings
 	w.writerow(firstline + ['transitions before onset', 'transitions after onset', 'average biased', 'average unbiased'])
 
+	#these are the hideous variables we will need
 	lastline = firstline
 	trans_before = 0
 	trans_after = 0
 	lines = 0
-	avg_biased = 0
-	avg_unbiased = 0
-	count = 0
+	trans_biased = 0
+	trans_unbiased = 0
+	num_biased = 0
+	num_unbiased = 0
 
 	#go through the csv file
 	for line in reader :
 		if lastline != firstline :
-			count += 1
 			if line[gaze_end_index] < line[speech_onset_index] :
 				trans_before += int(line[transition_index])
 			else :
 				trans_after += int(line[transition_index])
+			if line[biased_index] == 'y' :
+				trans_biased += int(line[transition_index])
+				num_biased += 1
+			else:
+				trans_unbiased += int(line[transition_index])
+				num_unbiased += 1
 			#if there's a new parcticipant
 			if line[participant_index] != lastline[participant_index] :
-				newline = lastline + [trans_before, trans_after, avg_biased, avg_unbiased]
+				newline = lastline + [trans_before, trans_after, trans_biased/num_biased, trans_unbiased/num_unbiased]
 				w.writerow(newline)
 				trans_before = 0
 				trans_after = 0
+				trans_unbiased = 0
+				trans_biased = 0
+				num_biased = 0
+				num_unbiased = 0
 			else:
 				w.writerow(lastline)
 		lastline = line
